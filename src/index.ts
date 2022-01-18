80e7a997ddb8639bb355e2653925c040d430aced
Prateek Verma <prateekverma70@gmail.com>
2022-01-18 19:56:06 +0530

    fix: added PORT

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => res.render("home"));
 app.get("/video", sendVideo);
